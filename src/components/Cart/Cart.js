c0f8d399c1c74a4adea9a37b785573e5e24f756f
Rezaul Karim <spaceled@gmail.com>
2021-10-26 22:10:05 +0600

    .....

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Cart = (props) => {
     const cart = props.cart;
@@ -34,6 +35,10 @@ const Cart = (props) => {
             <p><small>Shipping Cost: {shipping}</small></p>
             <p><small>Tax + VAT: {tax}</small></p>
             <p>Total Price: {total + shipping + tax}</p>
+            <br />
+            <Link to="/review">
+                <button className='main-button'>Review Order</button>
+            </Link>
         </div>
     );
 };
---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ReviewItem = (props) => {
+    console.log(props);
+    const {name, quantity} = props.product;
+    const reviewItemStyle = {
+        borderBottom: '1px solid lightgray',
+        marginBottom: '5px',
+        paddingBottom: '5px',
+        marginLeft: '200px'
+
+    }
+    return (
+        <div style={reviewItemStyle} className='review-item'>
+            <h4 className='product-name'>{name}</h4>
+            <p>Quantiry: {quantity}</p>
+            <br />
+            <button className='main-button'>Remove</button>
+        </div>
+    );
+};
+
+export default ReviewItem;
